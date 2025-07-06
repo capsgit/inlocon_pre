e148d14e2bbf048f201eeaf6dd22bcbf5debed4a
unaltrofigliodidio <christian.pineros@gmail.com>
2025-07-07 01:01:29 +0200

    "new path 6.0"

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/inlocon_pre/",
   build: {
-    outDir: "docs",
     assetsDir: "assets",
   },
   plugins: [react()],
