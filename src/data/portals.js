6a8132f332b61969e05fe4c6ce79bbc01d0eee97
unaltrofigliodidio <christian.pineros@gmail.com>
2025-07-07 01:32:32 +0200

    "new path last one"

--- src/data/portals.js
+++ src/data/portals.js
@@ -2,53 +2,53 @@ export const portals = [
   {
     title: "Bauportal Deutschland",
     url: "https://www.bauportal-deutschland.de",
-    img: "/inlocon_pre/assets/images/fas-khan-WJyuzi6EUTs-unsplash.jpg",
+    img: "/assets/images/fas-khan-WJyuzi6EUTs-unsplash.jpg",
   },
   {
     title: "Straßenbauportal",
     url: "https://www.strassenbauportal.de",
-    img: "/inlocon_pre/assets/images/maria-lupan-XeRqsvi9qBc-unsplash.jpg",
+    img: "/assets/images/maria-lupan-XeRqsvi9qBc-unsplash.jpg",
   },
 
   {
     title: "Cleanerportal",
     url: "https://www.cleanerportal.de",
-    img: "/inlocon_pre/assets/images/pexels-tima-miroshnichenko-6474202.jpg",
+    img: "/assets/images/pexels-tima-miroshnichenko-6474202.jpg",
   },
 
   {
     title: "Beschaffungsmarkt Office",
     url: "https://www.beschaffungsmarkt-office.de",
-    img: "/inlocon_pre/assets/images/pexels-fauxels-3184405.jpg",
+    img: "/assets/images/pexels-fauxels-3184405.jpg",
   },
   {
     title: "Container Modulbau",
     url: "https://www.container-modulbau.de",
-    img: "/inlocon_pre/assets/images/pedro-miranda-3QzMBrvCeyQ-unsplash.jpg",
+    img: "/assets/images/pedro-miranda-3QzMBrvCeyQ-unsplash.jpg",
   },
   {
     title: "Beschaffungsmarkt Fahrzeuge",
     url: "https://www.beschaffungsmarkt-fahrzeuge.de",
-    img: "/inlocon_pre/assets/images/maria-lupan-XeRqsvi9qBc-unsplash.jpg",
+    img: "/assets/images/maria-lupan-XeRqsvi9qBc-unsplash.jpg",
   },
   {
     title: "Energieausschreibungen",
     url: "https://www.energieausschreibungen.eu",
-    img: "/inlocon_pre/assets/images/thisisengineering-BU8lpW2Bn30-unsplash.jpg",
+    img: "/assets/images/thisisengineering-BU8lpW2Bn30-unsplash.jpg",
   },
   {
     title: "Beschaffungsmarkt Gesundheit",
     url: "https://www.beschaffungsmarkt-gesundheit.de",
-    img: "/inlocon_pre/assets/images/pexels-yankrukov-7691739.jpg",
+    img: "/assets/images/pexels-yankrukov-7691739.jpg",
   },
   {
     title: "Schienenverkehrsportal",
     url: "https://www.schienenverkehrsportal.de",
-    img: "/inlocon_pre/assets/images/pexels-ivan-drazic-20457695-30005142.jpg",
+    img: "/assets/images/pexels-ivan-drazic-20457695-30005142.jpg",
   },
   {
     title: "EE-Portal",
     url: "https://www.ee-portal.eu",
-    img: "/inlocon_pre/assets/images/pexels-shkrabaanthony-5244002.jpg",
+    img: "/assets/images/pexels-shkrabaanthony-5244002.jpg",
   },
 ];
